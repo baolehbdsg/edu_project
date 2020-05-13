87a6481f2af7789875ec4e8b08c0b595ed20ce68
gitacct_pm <gitacct_pm@163.com>
2020-05-13 22:52:30 +0800

    2020/5/13/

--- edu-admin/src/main/java/com/hebin/GmallAdminMainApplication.java
+++ edu-admin/src/main/java/com/hebin/GmallAdminMainApplication.java
@@ -1,10 +1,4 @@
-/**
- * Copyright (c) 2016-2019 谷粒开源 All rights reserved.
- *
- * https://www.guli.cloud
- *
- * 版权所有，侵权必究！
- */
+
 
 package com.hebin;
 
@@ -15,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GmallAdminMainApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GmallAdminMainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GmallAdminMainApplication.class, args);
+    }
 
 }
